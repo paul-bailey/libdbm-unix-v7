2699ee97ddbfab08ac71cbcd875b4e2e2f8b78bc
Paul Bailey <pbailey@dx4860>
2017-08-23 13:47:35 -0700

    Pull dbminit into its own C file.

--- libdbm/dbm.c
+++ libdbm/dbm.c
@@ -57,45 +57,6 @@ dbm_access(Database *db, long hash)
         }
 }
 
-Database *
-dbminit(char *file)
-{
-        struct stat statb;
-        Database *db;
-
-        db = malloc(sizeof(*db));
-        if (!db)
-                goto emalloc;
-
-        memset(db, 0, sizeof(*db));
-        strcpy(db->pagbuf, file);
-        strcat(db->pagbuf, ".pag");
-        db->pagfd = open(db->pagbuf, 2);
-        if (db->pagfd < 0)
-                goto epagfd;
-
-        strcpy(db->pagbuf, file);
-        strcat(db->pagbuf, ".dir");
-        db->dirfd = open(db->pagbuf, 2);
-        if (db->dirfd < 0)
-                goto edirfd;
-
-        fstat(db->dirfd, &statb);
-        db->maxbno = statb.st_size * BYTESIZ - 1;
-        db->access_oldb = -1;
-        db->getbit_oldb = -1;
-        return db;
-
-        /* close db->dirfd if more err proc. before this */
-edirfd:
-        close(db->pagfd);
-epagfd:
-        free(db);
-emalloc:
-        fprintf(stderr, "cannot init DBM\n");
-        return NULL;
-}
-
 long
 forder(Database *db, datum key)
 {
